dff8e1ffea51f1713bee5077c9ec5bbbe45be66b
ashutosh ambekar <ashuambekar@gmail.com>
2023-09-06 09:18:18 +0000

    commit on 6 sept 2023

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', (req, res, next) => {
     console.log('1!');
@@ -9,8 +13,14 @@ app.use('/', (req, res, next) => {
 
 app.use('/add-product', (req, res, next) => {
     console.log('2');
-    res.send('<h1>Add products page</h1>');
+    //res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit" tex="Add Product"></button></input></form>');
+    res.send('<h1>this is test add product page</h1>');
 });
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
 
 app.use('/', (req, res, next) => {
     console.log('3');
